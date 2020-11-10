74282e23ec5242e82301e251de8250cfaa6a0ff1
Adriane Hairston <adrih1110@comcast.net>
2020-11-09 21:20:24 -0800

    add login, signup pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,6 +13,9 @@ import { KitchenComponent } from './kitchen/kitchen.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
 import { RecipeItemEditComponent } from './recipes/recipe-item-edit/recipe-item-edit.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
@@ -23,10 +27,13 @@ import { RecipeItemEditComponent } from './recipes/recipe-item-edit/recipe-item-
     KitchenComponent,
     CalendarComponent,
     RecipeItemComponent,
-    RecipeItemEditComponent
+    RecipeItemEditComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     AppRoutingModule
   ],
