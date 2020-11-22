3a0510f69964b891b3e89d1e8e628d699dd8e859
Adriane Hairston <adrih1110@comcast.net>
2020-11-22 08:03:02 -0800

    added recipe add form

--- src/app/recipes/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-item/recipe-item.component.ts
@@ -4,6 +4,8 @@ import { RecipeService } from 'src/app/recipe.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Recipe } from 'src/app/shared/models/recipe.model';
 
+
+
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
@@ -21,6 +23,11 @@ export class RecipeItemComponent implements OnInit {
 
   ngOnInit(): void {
     this.myRecipe= this.recipeService.recipeSelected;
+  }
+
+  goToEdit(): void {
+    this.recipeService.recipeSelected = this.myRecipe;
+    this.router.navigateByUrl('/recipe-edit', {state: this.myRecipe});
   }
 
 }
