7f7df0a7a505c9c440d99e8843c18214c434e1a1
Adriane Hairston <adrih1110@comcast.net>
2020-11-26 09:15:58 -0800

    recipe_ingredient api calls

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,5 +3,6 @@ const router = require('express').Router()
 router.use('/auth', require('./auth'))
 router.use('/recipe', require('./recipe'))
 router.use('/ingredient', require('./ingredient'))
+router.use('/recipeIngredient', require('./recipeIngredient'))
 
 module.exports = router
