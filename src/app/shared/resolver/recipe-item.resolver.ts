7c6c3e4ad63fb496b1a5cf70f3b99fe0e6fb9152
Adriane Hairston <adrih1110@comcast.net>
2020-11-28 12:48:28 -0800

    recipe item display selected recipe

--- src/app/shared/resolver/recipe-item.resolver.ts
+++ src/app/shared/resolver/recipe-item.resolver.ts
@@ -3,14 +3,18 @@ import { Resolve } from '@angular/router';
 
 import { Observable } from 'rxjs';
 import { RecipeHttpService } from '../services/recipe.service';
+import { RecipeService } from 'src/app/recipe.service';
 
 @Injectable({providedIn: 'root'})
 export class RecipeItemResolver implements Resolve<Observable<string>> {
   
-  constructor(private recipeHttpService: RecipeHttpService) {}
+  constructor(
+      private recipeHttpService: RecipeHttpService,
+    private recipeService: RecipeService) {}
 
   resolve() {
-    const recipeId = 100; // replace with recipe service
+      // Get the recipeId from recipe service to make the API call
+     const recipeId =this.recipeService.recipeSelected.recipeId; 
     return this.recipeHttpService.getOne(recipeId);
   }
 }
