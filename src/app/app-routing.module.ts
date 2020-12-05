cba77a6009f659de547ebe77360546ea9b8ae2f6
dvastro <stroshid@oregonstate.edu>
2020-12-04 20:50:05 -0800

    Remove grocery list component from app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { CalendarComponent } from './calendar/calendar.component';
-import { GroceryListComponent } from './grocery-list/grocery-list.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { KitchenComponent } from './kitchen/kitchen.component';
 import { RecipesComponent } from './recipes/recipes.component';
@@ -20,7 +19,6 @@ const appRoutes: Routes = [
   { path: '', component: HomepageComponent, pathMatch: 'full' },
   { path: 'recipes', component: RecipesComponent, resolve: { message: RecipeResolver } },
   { path: 'kitchen', component: KitchenComponent, resolve: { message: KitchenResolver } },
-  { path: 'grocery-list', component: GroceryListComponent },
   { path: 'calendar', component: CalendarComponent },
   { path: 'recipe-item/:id', component: RecipeItemComponent, resolve: { message: RecipeItemResolver } },
   { path: 'recipe-edit/:id', component: RecipeItemEditComponent, resolve: { message: RecipeItemResolver } },
