a13c85ec6326a4c3477b5de04c76b130b0ebfd99
dvastro <stroshid@oregonstate.edu>
2020-12-05 15:00:02 -0800

    Clean up EventService

--- src/app/shared/services/event.service.ts
+++ src/app/shared/services/event.service.ts
@@ -4,7 +4,6 @@ import { Observable } from 'rxjs';
 import { catchError, first } from 'rxjs/operators';
 
 import { environment } from 'src/environments/environment';
-import { Ingredient } from '../models/ingredient.model';
 import { ErrorHandlerService } from './error-handler.service';
 import { Event } from '../models/event.model';
 
@@ -49,39 +48,4 @@ export class EventService {
       }
     }
   }
-
-  // populateCalendar(allIngredients: any[]) {
-  //   this.currentKitchen = new Map();
-
-  //   for (const currentIngredient of allIngredients) {
-  //     const currentCategory: string = currentIngredient.category;
-
-  //     if (this.currentKitchen.has(currentCategory)) {
-  //       const tempIngredientList: Ingredient[] = this.currentKitchen.get(currentCategory).slice();
-
-  //       tempIngredientList.push(
-  //         new Ingredient(
-  //           currentIngredient.ingredient_id,
-  //           currentIngredient.ingredient_name,
-  //           currentIngredient.quantity,
-  //           currentIngredient.unit,
-  //           currentIngredient.category
-  //         )
-  //       );
-
-  //       this.currentKitchen.set(currentCategory, tempIngredientList.slice());
-  //     } else {
-  //       const newIngredientList: Ingredient[] = [
-  //         new Ingredient(
-  //           currentIngredient.ingredient_id,
-  //           currentIngredient.ingredient_name,
-  //           currentIngredient.quantity,
-  //           currentIngredient.unit,
-  //           currentIngredient.category
-  //         )
-  //       ];
-  //       this.currentKitchen.set(currentCategory, newIngredientList.slice());
-  //     }
-  //   }
-  // }
 }
